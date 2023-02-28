dc160888df523af7e371c088f9e5f8a2fade7437
emanuel-vicino <126532249+emanuel-vicino@users.noreply.github.com>
2023-02-27 22:15:31 -0300

    Add files via upload

---
+++ curriculum/java/curriculum.js
@@ -0,0 +1,33 @@
+document.getElementById('datos1').onclick = function (){
+    console.log("mostrar datos")
+    document.getElementById('nombre').style.display = 'block'
+}
+document.getElementById('datos2').onclick = function(){
+    console.log('ocultar datos')
+    document.getElementById('nombre').style.display = 'none'
+}
+
+document.getElementById('experiencia 1').onclick = function (){
+    console.log("mostrar experiencia")
+    document.getElementById('parrafo').style.display = 'block'
+}
+document.getElementById('experiencia 2').onclick = function(){
+    console.log('ocultar experiencia')
+    document.getElementById('parrafo').style.display = 'none'
+}
+document.getElementById('habilidad 1').onclick = function (){
+    console.log("mostrar habilidades")
+    document.getElementById('lista').style.display = 'block'
+}
+document.getElementById('habilidad 2').onclick = function(){
+    console.log('ocultar habilidades')
+    document.getElementById('lista').style.display = 'none'
+}
+document.getElementById('contacto 1').onclick = function (){
+    console.log("mostrar datos de contacto")
+    document.getElementById('info').style.display = 'block'
+}
+document.getElementById('contacto 2').onclick = function(){
+    console.log('ocultar datos de contacto')
+    document.getElementById('info').style.display = 'none'
+}
